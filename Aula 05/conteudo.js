41ceb67749efa76e9effb59064a8a3c0fc5ca536
Fernanda H. Passoni <104275043+FernandaHaese@users.noreply.github.com>
2025-08-19 15:46:12 -0300

    Aula 05

---
+++ Aula 05/conteudo.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Aula 05</title>
+    <!--Script-->
+    <script>
+      alert("Mensagem de alerta");
+    </script>
+  </head>
+  <body>
+    <!--Heading-->
+    <h1>Bem-vindo</h1>
+    <h3>Está é minha página web</h3>
+    <!--Parágrafo-->
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <!--Imagem-->
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1aWEjSeRTnLdt5QtfEWE2Ul0DmJ6w6g9_9g&s"
+      alt="Imagem de um gato"
+      width="400px"
+    />
+    <h3>Top 3 doces</h3>
+    <!--Lista ordenada-->
+    <ol>
+      <li>Brownie</li>
+      <li>Cookie</li>
+      <li>Pudim</li>
+    </ol>
+    <h3>Meus pets</h3>
+    <!--Lista não ordenada-->
+    <ul>
+      <li>Luna</li>
+      <li>Romeu</li>
+      <li>Sofia</li>
+    </ul>
+    <!--Links-->
+    <a href="https://pt.wikipedia.org/wiki/Gato">Saiba mais sobre gatos</a>
+    <!--Quebra de linha-->
+    <br />
+    <br />
+    <!--Botão-->
+    <button>Botão</button>
+  </body>
+</html>
