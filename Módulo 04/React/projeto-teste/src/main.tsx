16daf98c58025b7ecbbade81bff791c41cd51f5b
lua <cardosomv@gmail.com>
2025-11-11 15:07:58 -0300

    Aula 29

--- Módulo 04/React/projeto-teste/src/main.tsx
+++ Módulo 04/React/projeto-teste/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import FotoArr from "./Aulas/Aula 29/main";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <FotoArr
+      raio="50%"
+      espessura={20}
+      cor="black"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAowaWEVRdppIjxA-EZYkQJF-XyMUaQFRNTw&s"
+    ></FotoArr>
+    <div></div>
+  </StrictMode>
+);
