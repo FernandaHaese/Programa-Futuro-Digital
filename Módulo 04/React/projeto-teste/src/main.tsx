ccb6b30138795a101f035b7347f64bc72d30f1e4
lua <cardosomv@gmail.com>
2025-12-02 14:50:45 -0300

    Ajuste Aula 35

--- Módulo 04/React/projeto-teste/src/main.tsx
+++ Módulo 04/React/projeto-teste/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Login from "./Aulas/Aula 35/main";
+import API from "./Aulas/Aula 35/main";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Login />
+    <API />
   </StrictMode>
 );
